0d0a1f0cdd52632143b3bfe54929a6631a220697
zerocoderalex <alex12449@mail.ru>
2025-01-12 17:57:52 +0500

    update set

--- Shopflowers/Shopflowers/settings.py
+++ Shopflowers/Shopflowers/settings.py
@@ -105,10 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'shop/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR/ 'shop/static'
+    BASE_DIR/ 'templates/static'
 ]
 
 # Default primary key field type
