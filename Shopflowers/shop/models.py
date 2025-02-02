92e78602ce62658eaf629c2df2d012bf8a5d10e7
zerocoderalex <alex12449@mail.ru>
2025-02-02 17:59:47 +0500

    corr

--- Shopflowers/shop/models.py
+++ Shopflowers/shop/models.py
@@ -35,8 +35,9 @@ class Order(models.Model):
     status = models.CharField(
         max_length=20,
         choices=[
-            ('pending', 'В ожидании'),
-            ('completed', 'Завершён')
+            ('pending', 'в ожидании'),
+            ('cancealed', 'отменен'),
+            ('completed', 'завершён')
         ],
         default='pending',
         verbose_name='Статус'
